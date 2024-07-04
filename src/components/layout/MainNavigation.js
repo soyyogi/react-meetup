a8d1ad0d5e731fc943cf8c2bfe82783bcc8d6ca4
Yogesh Yadav Devi <yogesh.yadav_ext@scrm.lidl>
2024-07-04 19:08:57 +0200

    [JIRA-XXXX] feat: add header animation on scroll

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,31 @@
+import { useState, useEffect, useCallback } from "react";
 import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
 
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation({ setPage }) {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (currentScrollTop > lastScrollTop) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+    setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+  }, [lastScrollTop]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop, handleScroll]);
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header className={`${classes.header} ${isVisible ? classes.visible : classes.hidden}`} data-test="navigation-header">
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
